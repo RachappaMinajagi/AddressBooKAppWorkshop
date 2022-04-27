d1976a172d5bac617b8a888859060635fe11e0e7
RACHOTI SM <rachotism@gmail.com>
2022-04-27 15:25:23 +0530

    Display the WellCome Message For AddressBookApplication

---
+++ src/main/java/com/bridgelabz/addressbook/AddressbookAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbookAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Addressbook Application WorkShop");
+		SpringApplication.run(AddressbookAppApplication.class, args);
+	}
+
+}
