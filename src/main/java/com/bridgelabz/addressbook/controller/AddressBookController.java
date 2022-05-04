236e02b227e9fa939d4512fbf461df748402e089
RACHOTI SM <rachotism@gmail.com>
2022-05-04 12:25:32 +0530

    Added: Using Swagger

--- src/main/java/com/bridgelabz/addressbook/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbook/controller/AddressBookController.java
@@ -14,24 +14,31 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+  /**
+   *  @RestController annotation in order to simplify the creation of RESTful web services.
+   *  Rest controller tells to spring boot that this my controller class in which create an API.
+  */
 @RestController
 @RequestMapping("/addressBook")
 public class AddressBookController {
-
+      /**
+       * @Autowired annotation is used for dependency injection.In spring boot application,
+       * all loaded beans are eligible for auto wiring to another bean.
+       */
 
     @Autowired
     private IAddressBookService addressBookService;
-
-
-
+      /**
+       *@PostMapping annotated methods handle the HTTP POST requests matched with given URI expression.
+       * @param : ResponseEntity
+       * @return :Ability to save employee details to repository
+       */
     @PostMapping(path = "/create")
     public ResponseEntity<String> addAddressBookData(@Valid @RequestBody AddressBookDTO addressBookDTO) {
         String newContact = addressBookService.createAddressBookData(addressBookDTO);
-        ResponseDTO respDTO = new ResponseDTO("New Contact Added in AddressBook ", newContact);
+        ResponseDTO respDTO = new ResponseDTO("New Contact Added in AddressBook ", newContact);//msg with data
         return new ResponseEntity (respDTO, HttpStatus.CREATED);
     }
-
-
 
     @GetMapping(value = "/retrieve/{token}")
     public ResponseEntity<ResponseDTO> getAddressBookDataById(@PathVariable String token)
@@ -40,8 +47,6 @@ public class AddressBookController {
         ResponseDTO dto = new ResponseDTO("Data retrieved successfully (:",listOfContacts);
         return new ResponseEntity(dto,HttpStatus.OK);
     }
-
-
 
     /**
      * get data for particular id
@@ -55,16 +60,12 @@ public class AddressBookController {
     }
 
 
-
     @PutMapping("/update/{token}")
     public ResponseEntity<String> updateRecordById(@PathVariable String token,@Valid @RequestBody AddressBookDTO addressBookDTO){
         AddressBookData entity = addressBookService.updateRecordByToken(token,addressBookDTO);
         ResponseDTO dto = new ResponseDTO("Address Book Record updated successfully",entity);
         return new ResponseEntity(dto,HttpStatus.ACCEPTED);
     }
-
-
-
 
     @DeleteMapping("/delete/{token}")
     public ResponseEntity<String> deleteRecordById(@PathVariable String token){
